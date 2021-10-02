0af9dc9d145bf7ed34e578b648973075996b4199
craus90 <craus902@gmail.com>
2021-10-02 23:13:17 +0400

    stats (not shown lol)

---
+++ Assets/Scripts/StatisticsPanel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatisticsPanel : Singletone<StatisticsPanel>
+{
+    public Text unitsCountText;
+    public Text starvedCountText;
+    public Text levelupsCountText;
+    public Text timeText;
+    public Text currentMissionText;
+
+    public void Update() {
+        unitsCountText.text = $"{GameManager.instance.worldView.creatureViews.Count}";
+        //starvedCountText.text = $"{GameManager.instance.worldView.creatureViews.Count}";
+        //levelupsCountText.text = $"{GameManager.instance.worldView.creatureViews.Count}";
+        //timeText.text = $"{GameManager.instance.worldView.creatureViews.Count}";
+        //currentMissionText.text = $"{GameManager.instance.worldView.creatureViews.Count}";
+    }
+}
